13afe7c09c6c4b29b8a19993d8ce4b2313353ab5
qwc-ch <zzzzzzxx2022@163.com>
2026-05-21 22:29:45 +0800

    修改友链

--- src/config/friendsConfig.ts
+++ src/config/friendsConfig.ts
@@ -108,8 +108,7 @@ export const friendsConfig: FriendLink[] = [
 		{
 		title: "ZhiJing’s Blog",
 		imgurl: "https://iwexe.top/avatar.svg",
-		desc: " Go with the flow.
-",
+		desc: " Go with the flow.",
 		siteurl: "https://iwexe.top/",
 		tags: ["Blog"],
 		weight: 100,
