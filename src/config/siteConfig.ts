4f627ab4f12c3293a75e54b15a1a078a4d333ce0
qwc-ch <zzzzzzxx2022@163.com>
2026-05-24 22:30:17 +0800

    修改图标

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -58,7 +58,7 @@ export const siteConfig: SiteConfig = {
 	favicon: [
 		{
 			// 图标文件路径
-			src: "/favicon/favicon.ico",
+			src: "favicon/favicon2.png",
 			// 可选，指定主题 'light' | 'dark'
 			// theme: "light",
 			// 可选，图标大小
