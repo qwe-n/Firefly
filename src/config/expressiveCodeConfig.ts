8ef54b1107457600cffb2622947b93715a54fffb
qwc-ch <zzzzzzxx2022@163.com>
2026-06-05 21:39:46 +0800

    修改代码块

--- src/config/expressiveCodeConfig.ts
+++ src/config/expressiveCodeConfig.ts
@@ -8,10 +8,10 @@ import type { ExpressiveCodeConfig } from "../types/config";
 
 export const expressiveCodeConfig: ExpressiveCodeConfig = {
 	// 暗色主题（用于暗色模式）
-	darkTheme: "one-dark-pro",
+	darkTheme: "github-dark-default",
 
 	// 亮色主题（用于亮色模式）
-	lightTheme: "one-light",
+	lightTheme: "github-dark-default",
 
 	// 更多样式请看expressive-code的官方文档
 	// https://expressive-code.com/guides/themes/
